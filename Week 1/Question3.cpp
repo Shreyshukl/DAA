443bfcc5c4aa705c2f1041e1b9c738928c694196
Shreyshukl <158249360+Shreyshukl@users.noreply.github.com>
2024-02-02 00:45:01 +0530

    Update Question3.cpp

--- Week 1/Question3.cpp
+++ Week 1/Question3.cpp
@@ -1,48 +1,62 @@
 #include<iostream>
-#include<cmath>
 using namespace std;
-int linearsearch(int arr[],int a, int b, int k)
+int High(int arr[],int n,int key)
 {
-    int i;
-    for(i=a;i<=b;i++)
+    if(arr[0]==key)
     {
-        if(arr[i]==k)
+        return key;
+    }
+    int i=1,high,low;
+    while(i<n && arr[i]<=key)
+    {
+        i=i*2;
+    }
+    return high=min(n-1,i);
+}
+int Low(int arr[],int n,int key)
+{
+    if(arr[0]==key)
+    {
+        return key;
+    }
+    int i=1,high,low;
+    while(i<n && arr[i]<=key)
+    {
+        i=i*2;
+    }
+    return low=i/2;
+}
+int LinearSearch(int arr[],int h,int l,int k)
+{
+    for(l;l<=h;l++)
+    {
+        if(l==k)
         {
-            return -1;
+            return 1;
         }
     }
     return 0;
 }
 int main()
 {
-    int arr[25],n,key,i,x,y;
-    cout<<"Enter the size of the array"<<endl;
+    int a[25],n,i,y,z,x,key;
+    cout<<"enetr limit";
     cin>>n;
-    cout<<"Enter the elements for the sorted array:"<< endl;
-    for(i=0; i<n; i++)
+    cout<<"enter values";
+    for(i=0;i<n;i++)
     {
-        cin>>arr[i];
+        cin>>a[i];
     }
-    cout<<"Enter the number to be searched:"<< endl;
+    cout<<"enter key";
     cin>>key;
-    if(arr[0]==key)
+    z=High(a,n,key);
+    x=Low(a,n,key);
+    y=LinearSearch(a,z,x,key);
+    if(y==1)
     {
-        cout<<"Key Present"<<endl;
-    }
-    i=1;
-    while(i<n && arr[i]<=key)
-    {
-        i=i*2;
-    }
-    x=min(i,n-1);
-    y=linearsearch(arr,i/2,x,key);
-    if(y==-1)
-    {
-        cout<<"Key present"<<endl;
+        cout<<"key found";
     }
     else
-    {
-        cout<<"Key not present"<<endl;
-    }
-    return 0;
+      cout<<"not found";
+return 0;
 }
